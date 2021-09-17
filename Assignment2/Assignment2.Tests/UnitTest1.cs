1f7f1356b987adab4f350b31ff9faab5d8852d53
rkri <rkri@itu.dk>
2021-09-17 13:02:06 +0200

    Created library

--- Assignment2/Assignment2.Tests/UnitTest1.cs
+++ Assignment2/Assignment2.Tests/UnitTest1.cs
@@ -1,14 +1,16 @@
 using System;
 using Xunit;
+using Library;
 
 namespace Assignment2.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Library_Student_Constructor_follows_rules()
         {
-
+            var student = new Student(12);
+            student.id = 2;
         }
     }
 }
---
+++ Assignment2/Library/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Library
+{
+    public class Student
+    {
+        public Student(int id){
+            Id = id;
+        }
+        public int Id{get;}
+        public string GivenName{get; set;}
+        public string SurName{get; set;}
+        public Status Status{get;}
+        public DateTime StartDate{get; set;}
+        public DateTime EndDate{get; set;}
+        public DateTime GraduationDate{get; set;}
+    }
+
+    public enum Status{
+        New, Active, Dropout, Graduated
+    }
+}
